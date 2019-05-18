0438273a798082dab36f7ed64517e135ebca6c01
mrtang <mrtang@nudt.edu.cn>
2019-05-18 08:37:31 +0800

    修改了命令解析

--- mecanum-kinova/RmCar_server2.py
+++ mecanum-kinova/RmCar_server2.py
@@ -70,8 +70,63 @@ class rmRemoteServer():
         tc_pro.start()
         self.kk = KinectClientV2019()
         self.s1 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        
+    
     def main(self):
+        going = 0
+        END = 0
+        fps_clock = pygame.time.Clock()
+        while not END:
+            try:
+                buf,addr = self.s.recvfrom(128)
+            except socket.error:
+                buf = None
+            
+            if buf is not None:
+                cmds = buf.split('##')
+                for cmd in cmds:
+                    b = cmd.split('**')
+                    if b[0] == 'pushtask':
+                        if b[1] == 'manul':
+                            cmd = b[2]
+                            self.rm.pushtask(cmd,'manul')
+                            print '\n>>> get manul task: %s'%(cmd)
+                        elif b[1] == 'auto':
+                            clas = b[2]
+                            p = np.fromstring(b[3],dtype = np.float32)
+                            x,y,w,h = box = np.fromstring(b[4],dtype = np.int32)
+                            rec = ((x,y),(w,h))
+                            self.track.init_tracker(box)
+                            self.rm.pushtask({'class':clas,'center':p},'auto')
+                            going = 1
+                            print '>>> get auto task: %s'%(clas)
+                        else:
+                            pass
+                    elif b[0] == 'stop':
+                        self.rm.pushtask('stop','manul')
+                    elif b[0] == 'quitpro':
+                        END = 1
+                        self.rm.pushtask('stop','manul')
+
+            if going:
+                box,pos = self.track.update()
+                self.s1.sendto(np.asarray(box,dtype=np.int32).tostring(),MAIN_ADDR2)
+
+                if clas == 'bottle':
+                    if pos['bottle'] is not None:   p = pos['bottle']
+                else:
+                    if pos['obj'] is not None:   p = pos['obj']
+
+                if self.rm.updatetask({'center':p},self.kk.point_cloud):
+                    going = 0
+                    print 'task completed'
+                    self.s1.sendto('**completed',BCI2000_ADDR)
+                else:
+                    pass
+
+            fps_clock.tick(10)
+        pygame.quit()
+    
+    def main1(self):
         going = 0
         END = 0
         fps_clock = pygame.time.Clock()
